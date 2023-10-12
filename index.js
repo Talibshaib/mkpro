406bbc759fee9c1e7733a63685160b6783e3511f
Talib Sheikh <mdtalib23038@gmail.com>
2023-10-12 18:00:53 +0530

    js added

---
+++ index.js
@@ -0,0 +1,38 @@
+// let urname = prompt("whats ur name");
+// alert("hey"+" "+ urname);
+// let para = prompt("write your Name");
+
+// let newpra= para.slice(0, 20);
+// alert(newpra);
+// let num = prompt("write a para");
+// let cnum = num.length;
+
+// alert("total para lenth " + cnum + " Remaining length " + (120 - cnum));
+// this is the convert upper and lower
+// let fname = prompt("write your fullname")
+// let uslice = fname.slice(0,1);
+// let lslice = fname.slice(1,fname.length);
+// let ufname = uslice.toUpperCase();
+// let lfname = lslice.toLowerCase();
+// alert("Hey "+ ufname+ lfname );
+
+// let dogage = prompt("what is your dog age");
+// let humnage = (dogage - 2) * 4 + 21;
+// alert("Your age is " + humnage);
+
+// function ratw() {
+//   console.log("how are you");
+//   console.log("how are you");
+//   console.log("how are you");
+//   console.log("how are you");
+//   console.log("how are you");
+//   console.log("how are you");
+//   console.log("how are you");
+//   console.log("how are you");
+// }
+
+// ratw();
+// let num = Math.random();
+// console.log(num);
+let newt =document.querySelector("button").classList;
+console.log(newt)
